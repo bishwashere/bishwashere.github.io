587e0d412d17845593a6b594290090b67b9beae0
bishwashere <b.bishwas@gmail.com>
2020-04-28 22:07:17 -0500

    changes

--- day6/js/decoratemytext.js
+++ day6/js/decoratemytext.js
@@ -1,29 +1,76 @@
-function biggerText(){
-    document.getElementById("textarea").style.fontSize="24pt";
-}
+// function biggerText(){
+//     document.getElementById("textarea").style.fontSize="24pt";
+// }
 
-function incrementText(){
-    var textSize = window.getComputedStyle(document.getElementById("textarea")).fontSize;
-    var increasedSize = parseInt(textSize)+2;
-    document.getElementById("textarea").style.fontSize=increasedSize+'pt';
-}
+// function incrementText(){
+//     var textSize = window.getComputedStyle(document.getElementById("textarea")).fontSize;
+//     var increasedSize = parseInt(textSize)+2;
+//     document.getElementById("textarea").style.fontSize=increasedSize+'pt';
+// }
 
-function intervalIncrement(){
-    var intervalIncrease = setInterval(function(){incrementText()}, 2000);
-}
+// function intervalIncrement(){
+//     setInterval(function(){incrementText()}, 2000);
+// }
 
-function intervalStop(){
-    clearTimeout(intervalIncrease);
+// function intervalStop(){
+    
+// }
 
-}
+// function alert(checkBoxElement){
+//     if (checkBoxElement.checked){
+//         document.getElementById("textarea").style.fontWeight="bold";
+//         document.getElementById("textarea").style.color="green";
+//         document.getElementById("textarea").style.textDecoration="underline";
+//     }
+//     else{
+//         document.getElementById("textarea").style="";
+//     }
+// }
 
-function alert(checkBoxElement){
-    if (checkBoxElement.checked){
-        document.getElementById("textarea").style.fontWeight="bold";
-        document.getElementById("textarea").style.color="green";
-        document.getElementById("textarea").style.textDecoration="underline";
+window.onload = function(){
+    "use strict";
+    // put all of your code here
+
+    const button1 = document.getElementById("button1");
+    button1.onclick=biggerText;
+
+    const button2 = document.getElementById("button2");
+    button2.onclick=incrementText;
+
+    const button3 = document.getElementById("button3");
+    button3.onclick=intervalIncrement;
+
+    const button4 = document.getElementById("button4");
+    button4.onclick=intervalStop;
+    
+    function biggerText(){
+        document.getElementById("textarea").style.fontSize="24pt";
     }
-    else{
-        document.getElementById("textarea").style="";
+    
+    function incrementText(){
+        var textSize = window.getComputedStyle(document.getElementById("textarea")).fontSize;
+        var increasedSize = parseInt(textSize)+2;
+        document.getElementById("textarea").style.fontSize=increasedSize+'pt';
+    }
+    
+    function intervalIncrement(){
+        window.myTimer = setInterval(incrementText, 2000);
+    }
+    
+    function intervalStop(){
+        clearTimeout(window.myTimer);
+    
+    }
+    
+    function alert(checkBoxElement){
+        if (checkBoxElement.checked){
+            document.getElementById("textarea").style.fontWeight="bold";
+            document.getElementById("textarea").style.color="green";
+            document.getElementById("textarea").style.textDecoration="underline";
+        }
+        else{
+            document.getElementById("textarea").style="";
+        }
     }
 }
+
