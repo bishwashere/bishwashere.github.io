c1c8f7662c68533cea8c8daab296acce4512c626
bishwashere <b.bishwas@gmail.com>
2020-05-01 02:52:37 -0500

    changes

--- w2d2/js/script.js
+++ w2d2/js/script.js
@@ -20,7 +20,7 @@ window.onload = function(){
             }(); //note
         }
     }
-    
+
     this.document.write("[6]");
     this.document.write("<br><br>");
     document.write("Increment: " + count.add())
@@ -50,8 +50,8 @@ window.onload = function(){
     // 8
     // ... 
 
-    var add = (function(){
-        var counter = 0;
+    const add = (function(){
+        let counter = 0;
         return function(){
             return counter +=1;
         }
@@ -59,8 +59,8 @@ window.onload = function(){
 
     // make_adder(inc)
 
-    var make_adder = (function(increment){
-        var counter=0;
+    let make_adder = (function(increment){
+        let counter=0;
         return function(){
             return counter +=increment;
         }
@@ -69,8 +69,8 @@ window.onload = function(){
     this.document.write("<br><br>");
     this.document.write("[8]");
     this.document.write("<br><br>");
-    add = make_adder(5)
-    document.write("Increment by 5: " + add())
+    const add2 = make_adder(5)
+    document.write("Increment by 5: " + add2())
     // this.document.write("<br>");
     // document.write(`Reset: ${count2.reset()}`)
 
@@ -78,5 +78,55 @@ window.onload = function(){
 
     // 10
     
+    const Person = function(){
+        // note - no variable declaration for each variables.
+        let   name = "Bishwas",
+                age = "26",
+                salary = 150000;
+
+        function getName(){
+            console.log(`Name: ${name}`);
+        };
+
+        function getAge(){
+            console.log(`Age: ${age}`);
+        }
+
+        function getSalary(){
+            console.log(`Salary: ${salary}`);
+        }
+
+        function setAge(newAge){
+            age = newAge;
+        }
+
+        function setSalary(newSalary){
+            salary = newSalary;
+            console.log(salary);
+        }
+
+        function setName(newName){
+            name = newName;
+        }
+
+        function increaseSalary(percentage){
+            setSalary(salary + ((percentage/100) * salary));
+        }
+
+        return {
+            increaseSalary: increaseSalary,
+        };
+    }();
+
+    // note - Revealing Module Pattern
+    // [1] Define all functions and variables.
+    // [2] Return an anonumous function with:
+    //    [2.1] pointer to private function
+    //    [2.2] 
+
+    document.write("<br><br>");
+    document.write("<br><br>");
+    document.write("[10] ( check console - F12 )");
+    Person.increaseSalary(5);
 
 }
