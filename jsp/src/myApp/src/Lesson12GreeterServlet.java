c2772259d3401ad8c69e62cc4fb23e363f18481f
bishwashere <b.bishwas@gmail.com>
2020-05-08 08:22:27 -0500

    servlet addition

---
+++ jsp/src/myApp/src/Lesson12GreeterServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(
+    urlPatterns = {
+        "/Lesson12GreeterServlet",
+        "/lesson12greeter"
+    }
+)
+public class Lesson12GreeterServlet extends HttpServlet {
+    
+    public void doGet(HttpServletRequest request, HttpServletResponse response) 
+    throws ServletException, IOException {
+        String fullName = request.getParameter("fullName");
+        StringBuilder sb = new StringBuilder();
+        sb.append("<h3> Hello, " + fullName + "</h3>");
+        response.setContentType("text/html");
+        response.getWriter().println(sb.toString());
+    }
+}
